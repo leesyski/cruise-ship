540ca385c71ddbfe098945d3b81d2a2781f07dd6
Mark Lees <marklees1@btinternet.com>
2019-03-10 18:44:33 +0000

    setsail

--- src/Ship.js
+++ src/Ship.js
@@ -1,5 +1,9 @@
 function Ship(startingPort) {
   this.startingPort = startingPort;
 }
+// eslint-disable-next-line func-names
+Ship.prototype.setSail = function () {
+  this.startingPort = 0;
+};
 
 module.exports = Ship;
