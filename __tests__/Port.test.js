445946075731fa953a9646497a6c60969313c8bf
Mark Lees <marklees1@btinternet.com>
2019-03-19 17:09:10 +0000

    Ports have ships

--- __tests__/Port.test.js
+++ __tests__/Port.test.js
@@ -9,4 +9,24 @@ describe('new port', () => {
     const port = new Port('Dover');
     expect(port.name).toBe('Dover');
   });
+  it('can add a ship', () => {
+    const port = new Port('Dover');
+    const ship = {};
+
+    port.addShip(ship);
+
+    expect(port.ship).toContain(ship);
+  });
+  it('can remove a ship', () => {
+    const port = new Port('Dover');
+    const titanic = {};
+    const queenMary = {};
+
+    port.addShip(titanic);
+    port.addShip(queenMary);
+    port.removeShip(queenMary);
+
+    expect(port.ship).toEqual([titanic]);
+  });
+
 });
--- src/Port.js
+++ src/Port.js
@@ -1,5 +1,14 @@
 function Port(name) {
   this.name = name;
+  this.ships = [];
 }
+
+Port.prototype.addShip = function addShip(ship) {
+  this.ships.push(ship);
+};
+
+Port.prototype.removeShip = function removeShip(shipToRemove) {
+  this.ships.splice(this.ships.findIndex(ship => ship === shipToRemove), 1);
+};
 
 module.exports = Port;
